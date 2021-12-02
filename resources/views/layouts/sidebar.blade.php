1a84a7b832bbcbe01408ef713c39193562439893
Shakil Hossain <shakiliit@innovationit.com.bd>
2021-12-02 09:51:03 +0600

    logout url set

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -91,7 +91,7 @@
                             <a class="dropdown-item active" href="#"><i data-feather="mail"></i> Messages</a>
                             <a class="dropdown-item" href="#"><i data-feather="settings"></i> Settings</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#"><i data-feather="log-out"></i> Logout</a>
+                            <a class="dropdown-item" href="{{ route('logout') }}"><i data-feather="log-out"></i> Logout</a>
                         </div>
                     </li>
                     <li class="dropdown">
